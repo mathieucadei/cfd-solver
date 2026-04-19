0229be1979d10935c31f9a1a1c5cc530ce9819ad
mathieucadei <215541433+mathieucadei@users.noreply.github.com>
2026-04-19 12:14:33 +0200

    refactor(setup): reformat exports

--- core/setup/__init__.py
+++ core/setup/__init__.py
@@ -1,3 +1,24 @@
-from .grids import make_x_grid, make_cole_hopf_1d_grid, compute_dx, compute_cole_hopf_dx, compute_dx_2d, compute_dy_2d
-from .time_stepping import compute_advective_dt, compute_convective_dt, compute_advective_dt_2d, compute_convective_dt_2d, compute_diffusive_dt, compute_diffusive_dt_2d, compute_cole_hopf_dt
-from .initial_conditions import hat_initial_condition_1d, hat_initial_condition_2d, hat_convective_initial_condition_2d, cole_hopf_initial_condition
+from .grids import (
+    compute_cole_hopf_dx, 
+    compute_dx, 
+    compute_dx_2d, 
+    compute_dy_2d,
+    make_cole_hopf_1d_grid, 
+    make_x_grid,
+    make_y_grid,  
+)                   
+from .initial_conditions import (
+    cole_hopf_initial_condition,
+    hat_convective_initial_condition_2d, 
+    hat_initial_condition_1d, 
+    hat_initial_condition_2d, 
+)
+from .time_stepping import (
+    compute_advective_dt,
+    compute_advective_dt_2d,
+    compute_cole_hopf_dt,  
+    compute_convective_dt, 
+    compute_convective_dt_2d, 
+    compute_diffusive_dt, 
+    compute_diffusive_dt_2d, 
+)
