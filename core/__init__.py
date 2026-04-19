e53f0386e494a068b7afb294062fc7e21cf3c702
mathieucadei <215541433+mathieucadei@users.noreply.github.com>
2026-04-19 12:24:32 +0200

    refactor(core): reformat exports in infrastructure order

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,58 @@
-from .config import Advection1DConfig, Advection2DConfig, Convection1DConfig, Convection2DConfig, Diffusion1DConfig, Diffusion2DConfig, BurgersEquation1DConfig, BurgersEquation2DConfig
-from .setup.grids import make_x_grid, make_cole_hopf_1d_grid, compute_dx, compute_dx_2d, compute_dy_2d
-from .setup.time_stepping import compute_convective_dt, compute_diffusive_dt, compute_advective_dt_2d, compute_convective_dt_2d, compute_cole_hopf_dt
-from .setup.initial_conditions import hat_initial_condition_1d, hat_initial_condition_2d, hat_convective_initial_condition_2d, cole_hopf_initial_condition
-from .numerics import solve_advection_1d, solve_advection_2d, solve_convection_1d, solve_convection_2d, solve_diffusion_1d, solve_diffusion_2d, solve_burgers_equation_1d, solve_burgers_equation_2d
-from .analytical import solve_heat_equation_1d, cole_hopf_1d_ufunc, solve_cole_hopf_1d
+from .config import (
+    Advection1DConfig, 
+    Advection2DConfig, 
+    Convection1DConfig, 
+    Convection2DConfig, 
+    Diffusion1DConfig, 
+    Diffusion2DConfig, 
+    BurgersEquation1DConfig, 
+    BurgersEquation2DConfig,
+)
+
+from .setup.grids import (
+    compute_cole_hopf_dx, 
+    compute_dx, 
+    compute_dx_2d, 
+    compute_dy_2d,
+    make_cole_hopf_1d_grid, 
+    make_x_grid,
+    make_y_grid,  
+)
+
+from .setup.time_stepping import (
+    compute_advective_dt,
+    compute_advective_dt_2d,
+    compute_cole_hopf_dt,  
+    compute_convective_dt, 
+    compute_convective_dt_2d, 
+    compute_diffusive_dt, 
+    compute_diffusive_dt_2d, 
+)
+
+from .setup.initial_conditions import (
+    cole_hopf_initial_condition,
+    hat_convective_initial_condition_2d, 
+    hat_initial_condition_1d, 
+    hat_initial_condition_2d, 
+)
+
+from .numerics import (
+    solve_advection_1d, 
+    solve_advection_2d,
+    solve_burgers_equation_1d, 
+    solve_burgers_equation_2d, 
+    solve_convection_1d, 
+    solve_convection_2d, 
+    solve_diffusion_1d, 
+    solve_diffusion_2d,
+)
+
+
 from .signal_processing import (
-    generate_mode_indices,
     compute_coefficients,
     compute_series_terms,
-    compute_series
+    compute_series,
+    generate_mode_indices,
 )
+
+from .analytical import cole_hopf_1d_ufunc, solve_cole_hopf_1d, solve_heat_equation_1d
--- core/setup/__init__.py
+++ core/setup/__init__.py
@@ -7,6 +7,7 @@ from .grids import (
     make_x_grid,
     make_y_grid,  
 )
+
 from .time_stepping import (
     compute_advective_dt,
     compute_advective_dt_2d,
@@ -15,7 +16,8 @@ from .time_stepping import (
     compute_convective_dt_2d, 
     compute_diffusive_dt, 
     compute_diffusive_dt_2d, 
-)               
+)
+              
 from .initial_conditions import (
     cole_hopf_initial_condition,
     hat_convective_initial_condition_2d, 
