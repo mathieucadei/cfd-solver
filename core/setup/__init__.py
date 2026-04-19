85071bb2bf1c655567b9b0100b3d2811d05d1af0
mathieucadei <215541433+mathieucadei@users.noreply.github.com>
2026-04-19 12:18:29 +0200

    refactor(setup): reformat exports in infrastructure order

--- core/setup/__init__.py
+++ core/setup/__init__.py
@@ -6,12 +6,6 @@ from .grids import (
     make_cole_hopf_1d_grid, 
     make_x_grid,
     make_y_grid,  
-)                   
-from .initial_conditions import (
-    cole_hopf_initial_condition,
-    hat_convective_initial_condition_2d, 
-    hat_initial_condition_1d, 
-    hat_initial_condition_2d, 
 )
 from .time_stepping import (
     compute_advective_dt,
@@ -21,4 +15,10 @@ from .time_stepping import (
     compute_convective_dt_2d, 
     compute_diffusive_dt, 
     compute_diffusive_dt_2d, 
+)               
+from .initial_conditions import (
+    cole_hopf_initial_condition,
+    hat_convective_initial_condition_2d, 
+    hat_initial_condition_1d, 
+    hat_initial_condition_2d, 
 )
