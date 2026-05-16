0e9489a3ee8c23584301c5105c2ec23b6685d738
mathieucadei <215541433+mathieucadei@users.noreply.github.com>
2026-05-16 22:58:39 +0200

    refactor(channel_flow): remove commented-out periodic boundary condition code

--- core/numerics/channel_flow.py
+++ core/numerics/channel_flow.py
@@ -66,60 +66,6 @@ def solve_channel_flow(
                         diffusion_v_term[1:-1, 1:-1])
         
         apply_periodic_channel_flow_boundary_2d(u, v, p, un, vn, nu, rho, config.source, dx, dy, dt)
-         
-        # # Periodic BC u @ x = 2     
-        # u[1:-1, -1] = (un[1:-1, -1] - un[1:-1, -1] * dt / dx * 
-        #             (un[1:-1, -1] - un[1:-1, -2]) -
-        #             vn[1:-1, -1] * dt / dy * 
-        #             (un[1:-1, -1] - un[0:-2, -1]) -
-        #             dt / (2 * rho * dx) *
-        #             (p[1:-1, 0] - p[1:-1, -2]) + 
-        #             nu * (dt / dx**2 * 
-        #             (un[1:-1, 0] - 2 * un[1:-1,-1] + un[1:-1, -2]) +
-        #             dt / dy**2 * 
-        #             (un[2:, -1] - 2 * un[1:-1, -1] + un[0:-2, -1])) + config.source * dt)
-
-        # # Periodic BC u @ x = 0
-        # u[1:-1, 0] = (un[1:-1, 0] - un[1:-1, 0] * dt / dx *
-        #             (un[1:-1, 0] - un[1:-1, -1]) -
-        #             vn[1:-1, 0] * dt / dy * 
-        #             (un[1:-1, 0] - un[0:-2, 0]) - 
-        #             dt / (2 * rho * dx) * 
-        #             (p[1:-1, 1] - p[1:-1, -1]) + 
-        #             nu * (dt / dx**2 * 
-        #             (un[1:-1, 1] - 2 * un[1:-1, 0] + un[1:-1, -1]) +
-        #             dt / dy**2 *
-        #             (un[2:, 0] - 2 * un[1:-1, 0] + un[0:-2, 0])) + config.source * dt)
-
-        # # Periodic BC v @ x = 2
-        # v[1:-1, -1] = (vn[1:-1, -1] - un[1:-1, -1] * dt / dx *
-        #             (vn[1:-1, -1] - vn[1:-1, -2]) - 
-        #             vn[1:-1, -1] * dt / dy *
-        #             (vn[1:-1, -1] - vn[0:-2, -1]) -
-        #             dt / (2 * rho * dy) * 
-        #             (p[2:, -1] - p[0:-2, -1]) +
-        #             nu * (dt / dx**2 *
-        #             (vn[1:-1, 0] - 2 * vn[1:-1, -1] + vn[1:-1, -2]) +
-        #             dt / dy**2 *
-        #             (vn[2:, -1] - 2 * vn[1:-1, -1] + vn[0:-2, -1])))
-
-        # # Periodic BC v @ x = 0
-        # v[1:-1, 0] = (vn[1:-1, 0] - un[1:-1, 0] * dt / dx *
-        #             (vn[1:-1, 0] - vn[1:-1, -1]) -
-        #             vn[1:-1, 0] * dt / dy *
-        #             (vn[1:-1, 0] - vn[0:-2, 0]) -
-        #             dt / (2 * rho * dy) * 
-        #             (p[2:, 0] - p[0:-2, 0]) +
-        #             nu * (dt / dx**2 * 
-        #             (vn[1:-1, 1] - 2 * vn[1:-1, 0] + vn[1:-1, -1]) +
-        #             dt / dy**2 * 
-        #             (vn[2:, 0] - 2 * vn[1:-1, 0] + vn[0:-2, 0])))   
-
-        # # Wall BC: u,v = 0 @ y = 0,2
-        # u[0, :] = 0
-        # u[-1, :] = 0
-        # v[0, :] = 0
-        # v[-1, :] = 0
 
         denominator = np.sum(np.abs(un))
 
