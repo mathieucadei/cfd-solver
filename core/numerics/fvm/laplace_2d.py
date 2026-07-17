418e27fdf486acc3aaf17d0096bfea2cb390fbf5
mathieucadei <215541433+mathieucadei@users.noreply.github.com>
2026-07-17 15:39:53 +0200

    feat: update solve_laplace_2d_fvm to accept initial condition as parameter

--- core/numerics/fvm/laplace_2d.py
+++ core/numerics/fvm/laplace_2d.py
@@ -7,7 +7,10 @@ from ...setup.initial_conditions import laplace_initial_condition_2d_fvm
 
 from dataclasses import dataclass, field
 
-def solve_laplace_2d_fvm(config: object):
+def solve_laplace_2d_fvm(
+        initial_condition: np.ndarray,        
+        config: object,
+):
 
     mesh = build_mesh(config)
 
@@ -19,35 +22,61 @@ def solve_laplace_2d_fvm(config: object):
     ax = area_x / dx
     ay = area_y / dy
 
-    phi = laplace_initial_condition_2d_fvm(config).ravel()
+    l1norm = 1
+
+    phi = initial_condition
     
     phi_n_x = phi[mesh['neigh_x']]
     phi_p_x = phi[mesh['owner_x']]
     phi_n_y = phi[mesh['neigh_y']]
     phi_p_y = phi[mesh['owner_y']]
 
-    face_fluxes_x = ax * (phi_n_x - phi_p_x)
+    phin_n_x = np.empty_like(phi_n_x)
+    phin_p_x = np.empty_like(phi_p_x)
+    phin_n_y = np.empty_like(phi_n_y)
+    phin_p_y = np.empty_like(phi_p_y)
 
-    face_fluxes_y = ay * (phi_n_y - phi_p_y)
+    history = []
 
-    lap_x = np.concatenate([face_fluxes_x[:,0].reshape(-1,1), 
-                            face_fluxes_x[:, 1:] - face_fluxes_x[:, :-1], 
-                            face_fluxes_x[:,-1].reshape(-1,1)], axis=1)
+    # while l1norm > config.l1_norm_target:
 
-    lap_y = np.concatenate([face_fluxes_y[0,:].reshape(-1,1).T, 
-                            face_fluxes_y[1:, :] - face_fluxes_y[:-1, :], 
-                            face_fluxes_y[-1,:].reshape(-1,1).T], axis=0)
+    phin_n_x = np.copy(phi_n_x)
+    phin_p_x = np.copy(phi_p_x)
+    phin_n_y = np.copy(phi_n_y)
+    phin_p_y = np.copy(phi_p_y)
 
-    lap = lap_x + lap_y
+    face_fluxes_x = ax * (phin_n_x - phin_p_x)
 
-    # laplacian = np.zeros((config.ny, config.nx))
+    face_fluxes_y = ay * (phin_n_y - phin_p_y)
+
+    # lap_x = np.concatenate([face_fluxes_x[:,0].reshape(-1,1), 
+    #                         face_fluxes_x[:, 1:] - face_fluxes_x[:, :-1], 
+    #                         - face_fluxes_x[:,-1].reshape(-1,1)], axis=1)
+
+    # lap_y = np.concatenate([face_fluxes_y[0,:].reshape(-1,1).T, 
+    #                         face_fluxes_y[1:, :] - face_fluxes_y[:-1, :], 
+    #                         - face_fluxes_y[-1,:].reshape(-1,1).T], axis=0)
+
+    # lap = lap_x + lap_y
+
+    laplacian = np.zeros((config.ny, config.nx))
     
-    # laplacian.flat[mesh['owner_x']] += face_fluxes_x
-    # laplacian.flat[mesh['neigh_x']] -= face_fluxes_x
-    # laplacian.flat[mesh['owner_y']] += face_fluxes_y
-    # laplacian.flat[mesh['neigh_y']] -= face_fluxes_y
+    laplacian.flat[mesh['owner_x']] += face_fluxes_x
+    laplacian.flat[mesh['neigh_x']] -= face_fluxes_x
+    laplacian.flat[mesh['owner_y']] += face_fluxes_y
+    laplacian.flat[mesh['neigh_y']] -= face_fluxes_y
 
-    laplacian = lap / mesh['V']
+        # denominator = np.sum(np.abs(pn))
+
+        # if denominator == 0:
+        #     l1norm = np.sum(np.abs(p) - np.abs(pn)) 
+        
+        # else:
+        #     l1norm = (np.sum(np.abs(p) - np.abs(pn))) / denominator
+        
+        # history.append(p.copy())
+
+    laplacian = laplacian / mesh['V']
 
     return laplacian
 
@@ -66,12 +95,14 @@ if __name__ == '__main__':
 
     mesh = build_mesh(Mesh)
 
-    laplacian = solve_laplace_2d_fvm(Mesh)
+    initial_condition = laplace_initial_condition_2d_fvm(Mesh)
 
-    print(laplacian)
+    # laplacian = solve_laplace_2d_fvm(initial_condition, Mesh)
+
+    print(initial_condition)
 
     fig, ax = plt.subplots()
-    pc = ax.pcolormesh(mesh['xf'], mesh['yf'], laplacian.T, edgecolors='k', linewidth=0.3)
+    pc = ax.pcolormesh(mesh['CX'], mesh['CY'], initial_condition.T, edgecolors='k', linewidth=0.3)
     fig.colorbar(pc, label='laplacian')
     ax.set_aspect('equal')
     plt.show()    
