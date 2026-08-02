5cc2819ccaee6e7e9dd9ab792440ba14ded42c13
mathieucadei <215541433+mathieucadei@users.noreply.github.com>
2026-08-02 19:19:48 +0200

    feat: refine 2D convection boundary condition calculations and adjust sigma for improved stability

--- core/numerics/fvm/operators.py
+++ core/numerics/fvm/operators.py
@@ -98,27 +98,30 @@ def compute_convection_2d_term(
 ) -> np.ndarray:
     """Compute the 2D upwind convection u & v terms"""
 
+    e_u = u**2 / 2
+    e_v = v**2 / 2
+
     u_term = np.zeros_like(u)
     v_term = np.zeros_like(v)
 
-    u_w = u[1:, :-1]
+    f_w_u = e_u[1:, :-1]
 
-    u_e = u[1:, 1:]
+    f_e_u = e_u[1:, 1:]
 
-    u_s = u[:-1, 1:]
+    f_s_u = v[:-1, 1:] * u[:-1, 1:]
 
-    u_n = u[1:, 1:]  
+    f_n_u = v[1:, 1:] * u[1:, 1:]  
 
-    v_w = v[1:, :-1]
+    f_w_v = u[1:, :-1] * v[1:, :-1]
 
-    v_e = v[1:, 1:]
+    f_e_v = u[1:, 1:] * v[1:, 1:]
 
-    v_s = v[:-1, 1:]
+    f_s_v = e_v[:-1, 1:]
 
-    v_n = v[1:, 1:]  
+    f_n_v = e_v[1:, 1:]  
 
-    u_term[1:, 1:] = dt * u[1:, 1:] * (u_e - u_w) / hx[1:] + dt * v[1:, 1:] * (u_n - u_s) / hy[1:]
+    u_term[1:, 1:] = dt * (f_e_u - f_w_u) / hx[1:] + dt * (f_n_u - f_s_u) / hy[1:]
 
-    v_term[1:, 1:] = dt * u[1:, 1:] * (v_e - v_w) / hx[1:] + dt * v[1:, 1:] * (v_n - v_s) / hy[1:]
+    v_term[1:, 1:] = dt * (f_e_v - f_w_v) / hx[1:] + dt * (f_n_v - f_s_v) / hy[1:]
 
     return u_term, v_term
