da68da9ee59d94cc023fc8af76249b47f7a2a35f
mathieucadei <215541433+mathieucadei@users.noreply.github.com>
2026-08-11 14:37:49 +0200

    feat: enhance 2D Laplace boundary condition calculations for improved accuracy and clarity

--- core/numerics/fvm/boundary_conditions.py
+++ core/numerics/fvm/boundary_conditions.py
@@ -205,110 +205,103 @@ def apply_laplace_boundary_2d(
 ) -> None:
     """Apply boundary updates for the 2D Laplace equation."""
 
-    f_w_u_bottom = face_areas_x[0, 1:-1] * (p[0, :-2]) / dist_x[:-1]
-    f_e_u_bottom  = face_areas_x[0, 2:] * (p[0, 2:]) / dist_x[1:]
-    f_sb_u = face_areas_y[0, 1:-1] * (bottom[1:-1]) / yc[0]
-    f_n_u_bottom  = face_areas_y[1, 1:-1] * (p[1, 1:-1]) / dist_y[0]
+    a_w_bottom = face_areas_x[0, 1:-1] / dist_x[:-1]
+    a_e_bottom = face_areas_x[0, 2:] / dist_x[1:]
+    a_sb = face_areas_y[0, 1:-1] / yc[0]
+    a_n_bottom = face_areas_y[1, 1:-1] / dist_y[0]   
 
-    p[0, 1:-1] = (((f_e_u_bottom + f_w_u_bottom) +  (f_n_u_bottom + f_sb_u)) / cell_volumes[0, 1:-1]) / \
-                ((face_areas_x[0, 1:-1] / dist_x[:-1] \
-                + face_areas_x[0, 2:] / dist_x[1:] \
-                + face_areas_y[0, 1:-1] / yc[0] \
-                + face_areas_y[1, 1:-1] / dist_y[0]) \
-                / cell_volumes[0, 1:-1])
+    f_w_bottom = a_w_bottom * p[0, :-2]
+    f_e_bottom = a_e_bottom * p[0, 2:]
+    f_sb = a_sb * bottom[1:-1]
+    f_n_bottom = a_n_bottom * p[1, 1:-1]
 
-    f_w_u_top = face_areas_x[-1, 1:-1] * (p[-1, :-2]) / dist_x[:-1]
-    f_e_u_top  = face_areas_x[-1, 2:] * (p[-1, 2:]) / dist_x[1:]
-    f_s_u_top = face_areas_y[-2, 1:-1] * (p[-2, 1:-1]) / dist_y[-1]
-    f_nb_u  = face_areas_y[-1, 1:-1] * (top[1:-1]) / (ly - yc[-1])
-
-    p[-1, 1:-1] = (((f_e_u_top + f_w_u_top)  +  (f_nb_u + f_s_u_top)) / cell_volumes[-1, 1:-1]) /  \
-                ((face_areas_x[-1, 1:-1] / dist_x[:-1] \
-                + face_areas_x[-1, 2:] / dist_x[1:] \
-                + face_areas_y[-2, 1:-1] / dist_y[-1] \
-                + face_areas_y[-1, 1:-1] / (ly - yc[-1])) \
-                / cell_volumes[-1, 1:-1])
-
-    f_wb_u = face_areas_x[1:-1, 0] * (left[1:-1]) / xc[0]
-    f_e_u_left  = face_areas_x[1:-1, 1] * (p[1:-1, 1]) / dist_x[0]
-    f_s_u_left = face_areas_y[1:-1, 0] * (p[:-2, 0]) / dist_y[:-1]
-    f_n_u_left  = face_areas_y[2:, 0] * (p[2:, 0]) / dist_y[1:]
-
-    
-    p[1:-1, 0] = (((f_e_u_left + f_wb_u)  +  (f_n_u_left + f_s_u_left))/ cell_volumes[1:-1, 0]) /  \
-                ((face_areas_x[1:-1, 0] / xc[0] \
-                + face_areas_x[1:-1, 1] / dist_x[0] \
-                + face_areas_y[1:-1, 0] / dist_y[:-1] \
-                + face_areas_y[2:, 0] / dist_y[1:]) \
-                / cell_volumes[1:-1, 0])
-
-    f_w_u_right= face_areas_x[1:-1, -2] * (p[1:-1, -2]) / dist_x[-1]
-    f_eb_u  = face_areas_x[1:-1, -1] * (right[1:-1]) / (lx - xc[-1])
-    f_s_u_right = face_areas_y[1:-1, -1] * (p[:-2, -1]) / dist_y[:-1]
-    f_n_u_right  = face_areas_y[2:, -1] * (p[2:, -1]) / dist_y[1:]
-    
-    p[1:-1, -1] = (((f_eb_u + f_w_u_right) + (f_n_u_right + f_s_u_right)) / cell_volumes[1:-1, -1]) /  \
-                ((face_areas_x[1:-1, -2] / dist_x[-1] \
-                + face_areas_x[1:-1, -1] / (lx - xc[-1]) \
-                + face_areas_y[1:-1, -1] / dist_y[:-1] \
-                + face_areas_y[2:, -1] / dist_y[1:]) \
-                / cell_volumes[1:-1, -1])
-
-    # f_wb_u_bottom_left = face_areas_x[0, 0] * (p[0, 0] - left[0]) / xc[0]
-    # f_e_u_bottom_left = face_areas_x[0, 1] * (p[0, 1] - p[0, 0]) / dist_x[0]
-    # f_sb_u_bottom_left = face_areas_y[0, 0] * (p[0, 0] - bottom[0]) / yc[0]
-    # f_n_u_bottom_left  = face_areas_y[1, 0] * (p[1, 0] - p[0, 0]) / dist_y[0]
-
-    # p[0, 0] = (f_e_u_bottom_left - f_wb_u_bottom_left) / cell_volumes[0, 0]  +  (f_n_u_bottom_left - f_sb_u_bottom_left) / cell_volumes[0, 0]
-
-    # f_wb_u_top_left = face_areas_x[-1, 0] * (p[-1, 0] - left[-1]) / xc[0]
-    # f_e_u_top_left  = face_areas_x[-1, 1] * (p[-1, 1] - p[-1, 0]) / dist_x[0]
-    # f_s_u_top_left = face_areas_y[-1, 0] * (p[-1, 0] - p[-2, 0]) / dist_y[-1]
-    # f_nb_u_top_left  = face_areas_y[-1, 0] * (top[-1] - p[-1, 0]) / (ly - yc[-1])
-
-    # p[-1, 0] = (f_e_u_top_left - f_wb_u_top_left) / cell_volumes[-1, 0]  +  (f_nb_u_top_left - f_s_u_top_left) / cell_volumes[-1, 0]
-
-    # f_w_u_bottom_right = face_areas_x[0, -1] * (p[0, -1] - p[0, -2]) / dist_x[-1]
-    # f_eb_u_bottom_right  = face_areas_x[0, -1] * (right[-1] - p[0, -1]) / (lx - xc[-1])
-    # f_sb_u_bottom_right = face_areas_y[0, -1] * (p[0, -1] - bottom[-1]) / yc[0]
-    # f_n_u_bottom_right  = face_areas_y[1, -1] * (p[1, -1] - p[0, -1]) / dist_y[-1]
-
-    # p[0, -1] = (f_eb_u_bottom_right - f_w_u_bottom_right) / cell_volumes[0, -1]  +  (f_n_u_bottom_right - f_sb_u_bottom_right) / cell_volumes[0, -1]
+    p[0, 1:-1] =(f_e_bottom + f_w_bottom + f_n_bottom + f_sb) / (a_w_bottom + a_e_bottom + a_sb + a_n_bottom)
 
 
-    # f_w_u_top_right = face_areas_x[-1, -1] * (p[-1, -1] - p[-1, -2]) / dist_x[-1]
-    # f_eb_u_top_right  = face_areas_x[-1, -1] * (right[-1] - p[-1, -1]) / (lx - xc[-1])
-    # f_s_u_top_right = face_areas_y[-2, -1] * (p[-1, -1] - p[-2, -1]) / dist_y[-1]
-    # f_nb_u_top_right  = face_areas_y[-1, -1] * (top[-1] - p[-1, -1]) / (ly - yc[-1])
+    a_w_top = face_areas_x[-1, 1:-1] / dist_x[:-1]
+    a_e_top = face_areas_x[-1, 2:] / dist_x[1:]
+    a_s_top = face_areas_y[-2, 1:-1] / dist_y[-1]
+    a_nb = face_areas_y[-1, 1:-1] / (ly - yc[-1])
+  
+    f_w_top = a_w_top * p[-1, :-2]
+    f_e_top = a_e_top * p[-1, 2:]
+    f_s_top = a_s_top * p[-2, 1:-1]
+    f_nb = a_nb * top[1:-1]
 
-    # p[-1, -1] = (f_eb_u_top_right - f_w_u_top_right) / cell_volumes[-1, -1]  +  (f_nb_u_top_right - f_s_u_top_right) / cell_volumes[-1, -1]
+    p[-1, 1:-1] =(f_e_top + f_w_top + f_nb + f_s_top) / (a_w_top + a_e_top + a_s_top + a_nb)
 
 
+    a_wb = face_areas_x[1:-1, 0] / xc[0]
+    a_e_left = face_areas_x[1:-1, 1] / dist_x[0]
+    a_s_left = face_areas_y[1:-1, 0] / dist_y[:-1]
+    a_n_left = face_areas_y[2:, 0] / dist_y[1:]
+  
+    f_wb = a_wb * left[1:-1]
+    f_e_left= a_e_left * p[1:-1, 1]
+    f_s_left = a_s_left * p[:-2, 0]
+    f_n_left = a_n_left * p[2:, 0]
 
-    # f_wb_u= face_areas_x[1:-1, 0] * (p[1:-1, 0] - left) / xc[0]
-    # f_e_u_left  = face_areas_x[1:-1, 1] * (p[1:-1, 1] - p[1:-1, 0]) / dist_x[1:]
-    # f_s_u_left = face_areas_y[1:-1, 0] * (p[1:-1, 0] - p[:-2, 0]) / dist_y[:-1, None]
-    # f_n_u_left  = face_areas_y[2:, 0] * (p[2:, 0] - p[1:-1, 0]) / dist_y[1:, None]
+    p[1:-1, 0] =(f_e_left + f_wb + f_n_left + f_s_left) / (a_wb + a_e_left + a_s_left + a_n_left)
 
-    
-    # p[1:-1, 0] = (f_e_u_left - f_wb_u) / cell_volumes[1:-1, 0]  +  (f_n_u_left - f_s_u_left) / cell_volumes[1:-1, 0]
 
-    # f_w_u_right= face_areas_x[1:-1, -2] * (p[1:-1, -1] - p[1:-1, -2]) / dist_x[-1]
-    # f_eb_u  = face_areas_x[1:-1, -1] * (right - p[1:-1, -1]) / (lx - xc[-1])
-    # f_s_u_right = face_areas_y[1:-1, -1] * (p[1:-1, -1] - p[:-2, -1]) / dist_y
-    # f_n_u_right  = face_areas_y[2:, -1] * (p[2:, -1] - p[1:-1, -1]) / dist_y[1:]
-    
-    # p[1:-1, -1] = (f_eb_u - f_w_u_right) / cell_volumes[1:-1, -1]  +  (f_n_u_right - f_s_u_right) / cell_volumes[1:-1, -1]
+    a_w_right = face_areas_x[1:-1, -2] / dist_x[-1]
+    a_eb = face_areas_x[1:-1, -1] / (lx - xc[-1])
+    a_s_right = face_areas_y[1:-1, -1] / dist_y[:-1]
+    a_n_right = face_areas_y[2:, -1] / dist_y[1:]
+  
+    f_w_right = a_w_right * p[1:-1, -2]
+    f_eb = a_eb * right[1:-1]
+    f_s_right = a_s_right * p[:-2, -1]
+    f_n_right = a_n_right * p[2:, -1]
 
-    # p[0, 0] = (f_e_u_bottom[0, 1] - f_wb_u[0,0]) / cell_volumes[0, 0]  +  (f_n_u_bottom - f_sb_u) / cell_volumes[0, 0]
+    p[1:-1, -1] =(f_eb + f_w_right + f_n_right + f_s_right) / (a_w_right + a_eb + a_s_right + a_n_right)
 
-    # p[-1, 0] = 0
+    a_wb_bottom = face_areas_x[0, 0] / xc[0]
+    a_e_bottom_left = face_areas_x[0, 1] / dist_x[0]
+    a_sb_left = face_areas_y[0, 0] / yc[0]
+    a_n_bottom_left = face_areas_y[1, 0] / dist_y[0]   
 
-    # p[0, -1] = 0
+    f_wb_bottom = a_wb_bottom * left[0]
+    f_e_bottom_left = a_e_bottom_left * p[0, 1]
+    f_sb_left = a_sb_left * bottom[0]
+    f_n_bottom_left = a_n_bottom_left * p[1, 0]
 
-    # p[-1, -1] = 0
+    p[0, 0] =(f_e_bottom_left + f_wb_bottom + f_n_bottom_left + f_sb_left) / (a_wb_bottom + a_e_bottom_left + a_sb_left + a_n_bottom_left)
 
-    # p[:, 0] = left  # p = left @ x = 0
-    # p[:, -1] = right  # p = right @ x = 2
-    # p[0, :] = bottom  # p = bottom @ y = 0
-    # p[-1, :] = top  # p = top @ y = 1
+    a_wb_top= face_areas_x[-1, 0] / xc[0]
+    a_e_top_left = face_areas_x[-1, 1] / dist_x[0]
+    a_s_top_left = face_areas_y[-1, 0] / dist_y[-1]
+    a_nb_left = face_areas_y[-1, 0] / (ly - yc[-1]) 
+
+    f_wb_top = a_wb_top * left[-1]
+    f_e_top_left = a_e_top_left * p[-1, 1]
+    f_s_top_left = a_s_top_left * p[-2, 0]
+    f_nb_left = a_nb_left * top[-1]
+
+    p[-1, 0] =(f_e_top_left + f_wb_top + f_nb_left + f_s_top_left) / (a_wb_top + a_e_top_left + a_s_top_left + a_nb_left)
+
+
+    a_w_bottom_right = face_areas_x[0, -2] / dist_x[-1]
+    a_eb_bottom = face_areas_x[0, -1] / (lx - xc[-1])
+    a_sb_right = face_areas_y[0, -1] / yc[0]
+    a_n_bottom_right = face_areas_y[1, -1] / dist_y[-1]   
+
+    f_w_bottom_right = a_w_bottom_right * p[0, -2]
+    f_eb_bottom = a_eb_bottom * right[-1]
+    f_sb_right = a_sb_right * bottom[-1]
+    f_n_bottom_right = a_n_bottom_right * p[1, -1]
+
+    p[0, -1] =(f_eb_bottom + f_w_bottom_right + f_n_bottom_right + f_sb_right) / (a_w_bottom_right + a_eb_bottom + a_sb_right + a_n_bottom_right)
+
+
+    a_w_top_right = face_areas_x[-1, -2] / dist_x[-1]
+    a_eb_top = face_areas_x[-1, -1] / (lx - xc[-1])
+    a_s_top_right = face_areas_y[-2, -1] / dist_y[-1]
+    a_nb_right = face_areas_y[-1, -1] / (ly - yc[-1])  
+
+    f_w_top_right = a_w_top_right * p[-1, -2]
+    f_eb_top = a_eb_top * right[-1]
+    f_s_top_right = a_s_top_right * p[-2, -1]
+    f_nb_right = a_nb_right * top[-1]
+
+    p[-1, -1] =(f_eb_top + f_w_top_right + f_nb_right + f_s_top_right) / (a_w_top_right + a_eb_top + a_s_top_right + a_nb_right)
